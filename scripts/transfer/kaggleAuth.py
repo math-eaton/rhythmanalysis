9c7ec5fd22a5880029a680b076860c8077c89a89
math-eaton <mjh2241@columbia.edu>
2025-05-06 11:54:35 -0400

    transfer learning osx stresstest

--- scripts/transfer/kaggleAuth.py
+++ scripts/transfer/kaggleAuth.py
@@ -5,6 +5,8 @@ kagglehub.login() # This will prompt you for your credentials.
 # We also offer other ways to authenticate (credential file & env variables): https://github.com/Kaggle/kagglehub?tab=readme-ov-file#authenticate
 
 # Download latest version
-path = kagglehub.model_download("google/yamnet/tensorFlow2/yamnet")
+# path = kagglehub.model_download("google/yamnet/tensorFlow2/yamnet")
+# path = kagglehub.model_download("google/yamnet/tfLite/classification-tflite")
+path = kagglehub.model_download("google/yamnet/tfLite/tflite")
 
 print("Path to model files:", path)
